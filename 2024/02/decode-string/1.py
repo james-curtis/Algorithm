fb9735d32e13583f8857924bf1e0d89ee04b06a6
Nancy Harris <jamescurtis@foxmail.com>
2024-02-06 00:28:23 +0800

    update

--- 2024/02/decode-string/1.py
+++ 2024/02/decode-string/1.py
@@ -7,18 +7,27 @@ class Solution:
         # 找到第一个 ]
         right = s.find(']')
         # 找到前面的 [
-        left = s.rfind('[', 0, right)
+        left = s.rfind('[',0,right)
         # 匹配前面的数字
         num = ''
-        idx = left
+        idx=left
         while True:
-            idx -= 1
+            idx-=1
             if s[idx].isdigit() is False:
                 break
             num = s[idx] + num
         # 展开
-        flatten = s[left + 1:right] * int(num)
-        res = s[:idx + 1] + flatten + s[right + 1:]
+        flatten = s[left+1:right]*int(num)
+        res = s[:idx+1] + flatten + s[right+1:]
         # 递归
         print(flatten, res)
+        return self.decodeString(res)
+
+import re
+class Solution:
+    def decodeString(self, s: str) -> str:
+        if s.find(']') == -1:
+            return s
+        match = re.search(r'(\d+)\[(\w+)\]', s)
+        res = s.replace(match[0], match[2] * int(match[1]))
         return self.decodeString(res)
