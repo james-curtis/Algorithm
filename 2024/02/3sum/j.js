0f18d54c3f15497cc2c71985c213a66e5049c5d8
Nancy Harris <jamescurtis@foxmail.com>
2024-02-14 17:15:08 +0800

    update

--- 2024/02/3sum/j.js
+++ 2024/02/3sum/j.js
@@ -5,23 +5,26 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-    let map = new Map()
-    for (let i = 0; i < nums.length; i++) {
-        map.set(nums[i], i);
-    }
-
-    let res = new Set()
-    for (let i = 0; i < nums.length - 1; i++) {
-        for (let j = 1; j < nums.length; j++) {
-            const need = 0 - nums[i] - nums[j];
-            if (!map.has(need)) {
-                continue;
+    const list = nums.sort((a, b) => a - b);
+    const ans = [];
+    for (let i = 0; i < list.length - 2; i++) {
+        if (i !== 0 && list[i] === list[i - 1])
+            continue;
+        for (let j = i + 1, k = list.length - 1; j < list.length - 1; j++) {
+            if (j !== i + 1 && list[j] === list[j - 1])
+                continue
+            for (; j < k && list[i] + list[j] + list[k] > 0; k--) ;
+            if (j === k)
+                break;
+            if (list[i] + list[j] + list[k] === 0) {
+                ans.push([list[i], list[j], list[k]]);
             }
-            res.add([need, nums[i], nums[j]].sort().join(','));
         }
     }
-    return Array.from(res).map(e => e.split(',').map(Number));
+    return ans;
 };
 
-nums = [-1,0,1,2,-1,-4]
-console.log(threeSum(nums));
+// nums = [-1, 0, 1, 2, -1, -4]
+// nums = [-2, 0, 0, 2, 2]
+// nums = [-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4]
+// console.log(threeSum(nums));
