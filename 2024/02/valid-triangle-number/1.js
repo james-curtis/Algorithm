fc25a35fff269b2041685ad97f19f9533c709394
Nancy Harris <jamescurtis@foxmail.com>
2024-02-18 00:05:32 +0800

    update

--- 2024/02/valid-triangle-number/1.js
+++ 2024/02/valid-triangle-number/1.js
@@ -5,26 +5,22 @@
  * @return {number}
  */
 var triangleNumber = function (nums) {
+    nums.sort();
     let ans = 0;
     let res = [];
     for (let i = 0; i < nums.length - 2; i++) {
-        for (let j = i + 1; j < nums.length - 1; j++) {
-            for (let k = j + 1; k < nums.length; k++) {
-                const [a, b, c] = [nums[i], nums[j], nums[k]].toSorted();
-                if (c !== 0
-                    && a + b > c
-                    && c + a > b
-                    && b + c > a) {
-                    ans++
-                    // res.push([nums[i], nums[j], nums[k]]);
-                }
+        for (let l = i + 1, r = nums.length - 1; l !== r;) {
+            if (nums[l] + nums[r] > nums[i]) {
+                ans += r - l;
+                r--
+            } else {
+                l++;
             }
         }
     }
-    console.log(res);
     return ans;
 };
 
-var nums = [2, 2, 3, 4]
+// nums = [2, 2, 3, 4]
 nums = [24, 3, 82, 22, 35, 84, 19]
 console.log(triangleNumber(nums));
