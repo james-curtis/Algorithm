7c3fa9a08c84e2e4c0ae94babd60b3580b0feb31
Nancy Harris <jamescurtis@foxmail.com>
2024-03-20 17:51:34 +0800

    update

--- 2024/03/kth-largest-element-in-an-array/1.js
+++ 2024/03/kth-largest-element-in-an-array/1.js
@@ -5,8 +5,37 @@
  * @param {number} k
  * @return {number}
  */
-var findKthLargest = function(nums, k) {
-    let prev = null;
-    let cursor = null;
+var findKthLargest = function (nums, k) {
+    let ans = null;
+    const targetKey = nums.length - k;
 
+    function quick(nums) {
+        function helper(l, r) {
+            if (l >= r) return;
+            // random
+            let rand = ~~(Math.random() * (r - l)) + l;
+            [nums[l], nums[rand]] = [nums[rand], nums[l]];
+            // random end
+            let p = l;
+            for (let i = l + 1; i <= r; i++) {
+                if (nums[i] < nums[l])
+                    [nums[i], nums[p]] = [nums[++p], nums[i]];
+            }
+            [nums[l], nums[p]] = [nums[p], nums[l]];
+            if (p < targetKey) {
+                helper(p + 1, r);
+            } else if (p > targetKey) {
+                helper(l, p - 1);
+            }
+        }
+
+        helper(0, nums.length - 1);
+    }
+
+    quick(nums);
+    return nums[targetKey];
 };
+
+a = [3, 2, 3, 1, 2, 4, 5, 5, 6];
+val = findKthLargest(a, 4);
+console.log(val);
