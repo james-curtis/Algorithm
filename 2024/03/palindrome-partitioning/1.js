48f661c7567c57a85be7529c7fb5c6b068f9dfaa
Nancy Harris <jamescurtis@foxmail.com>
2024-03-26 19:54:47 +0800

    update

---
+++ 2024/03/palindrome-partitioning/1.js
@@ -0,0 +1,17 @@
+// https://leetcode.cn/problems/palindrome-partitioning/?envType=study-plan-v2&envId=top-100-liked
+rl = require('readline').createInterface({input: process.stdin});
+readline = () => new Promise(res => rl.on('line', res));
+
+/**
+ * @param {string} s
+ * @return {string[][]}
+ */
+var partition = function (s) {
+    let ans = [];
+
+    function dfs(s) {
+
+    }
+
+    return ans
+};
