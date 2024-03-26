099c1d8cb63c71ffbcaad930c666a49eed0a9b39
Nancy Harris <jamescurtis@foxmail.com>
2024-03-26 22:13:32 +0800

    update

--- 2024/03/all-paths-from-source-to-target/1.js
+++ 2024/03/all-paths-from-source-to-target/1.js
@@ -23,5 +23,49 @@ var allPathsSourceTarget = function (graph) {
     return ans;
 };
 
-res = allPathsSourceTarget([[2], [], [1]]);
-console.log(res);
+// floyd
+var allPathsSourceTarget = function (graph) {
+    const blankMatrix = Array.from({length: graph.length}).map(e => Array.from({length: graph.length}).fill(Infinity))
+    const matrix = structuredClone(blankMatrix);
+
+    for (let i = 0; i < graph.length; i++) {
+        matrix[i][i] = 0;
+        for (let j = 0; j < graph[i].length; j++) {
+            matrix[i][graph[i][j]] = 1;
+        }
+    }
+
+    function floyd(matrix) {
+        const dist = matrix;
+        const path = structuredClone(blankMatrix);
+
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix.length; j++) {
+                if (Object.is(matrix[i][j], Infinity)) {
+                    path[i][j] = null;
+                } else {
+                    path[i][j] = j;
+                }
+            }
+        }
+
+        for (let candidate = 0; candidate < matrix.length; candidate++) {
+            for (let i = 0; i < matrix.length; i++) {
+                for (let j = 0; j < matrix.length; j++) {
+                    if (dist[i][j] > dist[i][candidate] + dist[candidate][j]) {
+                        dist[i][j] = dist[i][candidate] + dist[candidate][j];
+                        path[i][j] = path[i][candidate];
+                    }
+                }
+            }
+        }
+        return {dist, path}
+    }
+
+    floyd(matrix);
+
+    let ans = [];
+    // 好像不行了，只能找到最短路
+
+
+};
