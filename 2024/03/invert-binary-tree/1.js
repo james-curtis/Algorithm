360ad8c2a5acc8688ec31c2b580271b573d1f43b
Nancy Harris <jamescurtis@foxmail.com>
2024-03-27 22:23:15 +0800

    update

---
+++ 2024/03/invert-binary-tree/1.js
@@ -0,0 +1,17 @@
+// https://leetcode.cn/problems/invert-binary-tree/solutions/415160/fan-zhuan-er-cha-shu-by-leetcode-solution/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+
+};
