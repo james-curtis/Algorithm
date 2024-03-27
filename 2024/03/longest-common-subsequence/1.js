c3e7e1f818f662e807c4b4ab9cddec6fc4d0c696
Nancy Harris <jamescurtis@foxmail.com>
2024-03-27 13:22:25 +0800

    update

--- 2024/03/longest-common-subsequence/1.js
+++ 2024/03/longest-common-subsequence/1.js
@@ -5,6 +5,17 @@
  * @param {string} text2
  * @return {number}
  */
-var longestCommonSubsequence = function(text1, text2) {
+var longestCommonSubsequence = function (text1, text2) {
+    const dp = Array.from({length: text1.length + 1}).map(e => Array.from({length: text2.length + 1}).fill(0));
 
+    for (let i = 0; i < text1.length; i++) {
+        for (let j = 0; j < text2.length; j++) {
+            if (text1[i] === text2[j]) {
+                dp[i + 1][j + 1] = dp[i][j] + 1;
+                continue;
+            }
+            dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+        }
+    }
+    return dp[text1.length][text2.length];
 };
